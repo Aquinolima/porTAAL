eeed02cfbf1e2fd01cd17bd8365bb07e9d4f21f4
Thiago Aquino <aquinolima.thiago@gmail.com>
2020-10-18 20:11:02 -0300

    :tada: initial commit :tada:

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="pt-Br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>porTAAL Arquitetura</title>
+    <link rel="stylesheet" href="_assets/_css/style.css">
+
+</head>
+
+<body>
+
+    <header></header>
+
+    <section></section>
+
+    <footer></footer>
+    
+    <script type="text/javascript" src="_assets/_script/script.js"></script>
+  
+</body>
+
+</html>
